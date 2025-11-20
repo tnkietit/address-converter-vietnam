ec7dccd71156fca8da115c15c8e044a3e91b8d6d
tnkietit <tnkiet.it@gmail.com>
2025-11-20 19:07:31 +0700

    Add TypeScript types and main app files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AdminPage from './pages/AdminPage'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
